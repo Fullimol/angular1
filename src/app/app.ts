5199630acf9f2b01b97964098a6a81282edf4151
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-08-20 21:24:31 -0300

    blindeos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   nombre: string = "Bruno";
   contador = 0;
+
+  link = "https://www.google.com";
+  estilo = "background-color: lightblue;";
+
+  contadorSumar() {
+    this.contador++;
+  }
 }
