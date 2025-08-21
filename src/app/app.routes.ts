1f60ceb1d129921d78ccc3e2c26fef8063c84313
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-08-21 19:14:44 -0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,24 @@
 import { Routes } from '@angular/router';
 import { Bienvenida } from './bienvenida/bienvenida';
+import { Blindeos } from './blindeos/blindeos';
+import { Error } from './error/error';
 
 export const routes: Routes = [{
     path: 'bienvenida',
     component: Bienvenida
+},
+{
+    path: 'blindeos',
+    component: Blindeos
+},
+{
+    path: 'error',
+    component: Error
+}, {
+    path: 'login',
+    loadComponent: () => import('./login/login').then(m => m.Login) // Esto es para lazy loading. Nos permite cargar el componente solo cuando es necesario.           (HACERLO SIEMPRE DE ESTA FORMA, el "component" no es tan eficiente.)
+},
+{
+    path: '**', // Esto siempre va en la ultima posicion.
+    redirectTo: "error"
 }];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { Blindeos } from "./blindeos/blindeos";
-import { Bienvenida } from "./bienvenida/bienvenida";
-import { Error } from "./error/error";
-import { Login } from "./login/login";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Blindeos, Bienvenida, Error, Login, RouterLink],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
